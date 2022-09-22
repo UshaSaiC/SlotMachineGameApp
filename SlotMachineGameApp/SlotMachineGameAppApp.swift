6d400eb5d7f48b71b26312461c7c422c599bd14d
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-23 00:00:51 +0530

    Initial Commit

---
+++ SlotMachineGameApp/SlotMachineGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineGameAppApp.swift
+//  SlotMachineGameApp
+//
+//  Created by Usha Sai Chintha on 23/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
