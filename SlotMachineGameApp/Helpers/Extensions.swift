cb8d5bca5e51bc3d0bdac168b76fb6be5dc61405
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-23 09:10:23 +0530

    Updated home view to show scores

---
+++ SlotMachineGameApp/Helpers/Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Extensions.swift
+//  SlotMachineGameApp
+//
+//  Created by Usha Sai Chintha on 23/09/22.
+//
+
+import SwiftUI
+
+extension Text {
+    func scoreLabelStyle() -> Text {
+        self
+            .foregroundColor(.white)
+            .font(.system(size: 10, weight: .bold, design: .rounded))
+    }
+    
+    func scoreNumberStyle() -> Text {
+        self
+            .foregroundColor(.white)
+            .font(.system(.title, design: .rounded))
+            .fontWeight(.heavy)
+    }
+}
