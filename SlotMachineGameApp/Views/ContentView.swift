167fd757fb2ea361f79f04fd1d0f640da27809c2
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-23 12:22:59 +0530

    Added the gaming logic and bettings view on home screen

--- SlotMachineGameApp/Views/ContentView.swift
+++ SlotMachineGameApp/Views/ContentView.swift
@@ -9,7 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let symbols = ["gfx-bell", "gfx-cherry", "gfx-coin", "gfx-grape", "gfx-seven", "gfx-strawberry"]
+    
+    @State private var highScore: Int = 0
+    @State private var coins: Int = 100
+    @State private var betAmount: Int = 10
+    @State private var reels: Array = [0, 1, 2]
     @State private var showingInfoView: Bool = false
+    @State private var isActiveBet10: Bool = true
+    @State private var isActiveBet20: Bool = false
+    
+    func spinReels() {
+        
+        // reels[0] = Int.random(in: 0...symbols.count-1)
+        // reels[1] = Int.random(in: 0...symbols.count-1)
+        // reels[2] = Int.random(in: 0...symbols.count-1)
+        
+        // above lines can generate random integers.. but we can simplify this by using map as in below code
+        reels = reels.map({ _ in
+            Int.random(in: 0...symbols.count-1)
+        })
+    }
+    
+    func checkWinning() {
+        if reels[0] == reels[1] && reels[1] == reels[2] {
+            playerWins()
+            if coins > highScore {
+                newHighScore()
+            }
+            
+        } else {
+            playerLoses()
+        }
+    }
+    
+    func playerWins() {
+        coins += betAmount * 10
+    }
+    
+    func newHighScore(){
+        highScore = coins
+    }
+    
+    func playerLoses() {
+        coins -= betAmount
+    }
+    
+    func activateBet20() {
+        betAmount = 20
+        isActiveBet20 = true
+        isActiveBet10 = false
+    }
+    
+    func activateBet10() {
+        betAmount = 10
+        isActiveBet20 = false
+        isActiveBet10 = true
+    }
     
     var body: some View {
         ZStack {
@@ -29,7 +85,7 @@ struct ContentView: View {
                         Text("Your\nCoins".uppercased())
                             .scoreLabelStyle()
                             .multilineTextAlignment(.trailing)
-                        Text("100")
+                        Text("\(coins)")
                             .scoreNumberStyle()
                             .modifier(ScoreNumberModifier())
                     }
@@ -39,7 +95,7 @@ struct ContentView: View {
                     Spacer()
                     
                     HStack {
-                        Text("200")
+                        Text("\(highScore)")
                             .scoreNumberStyle()
                             .modifier(ScoreNumberModifier())
                         Text("High\nScore".uppercased())
@@ -52,7 +108,7 @@ struct ContentView: View {
                 VStack(alignment: .center, spacing: 0){
                     ZStack {
                         ReelView()
-                        Image("gfx-bell")
+                        Image(symbols[reels[0]])
                             .resizable()
                             .modifier(ImageModifier())
                     }
@@ -60,7 +116,7 @@ struct ContentView: View {
                     HStack(alignment: .center, spacing: 0){
                         ZStack {
                             ReelView()
-                            Image("gfx-seven")
+                            Image(symbols[reels[1]])
                                 .resizable()
                                 .modifier(ImageModifier())
                         }
@@ -69,7 +125,7 @@ struct ContentView: View {
                         
                         ZStack {
                             ReelView()
-                            Image("gfx-cherry")
+                            Image(symbols[reels[2]])
                                 .resizable()
                                 .modifier(ImageModifier())
                         }
@@ -77,7 +133,8 @@ struct ContentView: View {
                     .frame(maxWidth: 500)
                     
                     Button {
-                        
+                        self.spinReels()
+                        self.checkWinning()
                     } label: {
                         Image("gfx-spin")
                             .resizable()
@@ -92,16 +149,18 @@ struct ContentView: View {
                 
                 HStack {
                     HStack(alignment: .center, spacing: 0) {
-                        Button {} label: {
+                        Button {
+                            self.activateBet20()
+                        } label: {
                             Text("20")
                                 .fontWeight(.heavy)
-                                .foregroundColor(.white)
+                                .foregroundColor(isActiveBet20 ? Color("ColorYellow") : .white)
                                 .modifier(BetNumberModifier())
                         }
                         .modifier(BetCapsuleModifier())
                         Image("gfx-casino-chips")
                             .resizable()
-                            .opacity(0)
+                            .opacity(isActiveBet20 ? 1 : 0)
                             .modifier(CasinoChipsModifier())
                             .padding(5)
                     }
@@ -111,13 +170,15 @@ struct ContentView: View {
                     HStack(alignment: .center, spacing: 0) {
                         Image("gfx-casino-chips")
                             .resizable()
-                            .opacity(1)
+                            .opacity(isActiveBet10 ? 1 : 0)
                             .modifier(CasinoChipsModifier())
                             .padding(5)
-                        Button {} label: {
+                        Button {
+                            self.activateBet10()
+                        } label: {
                             Text("10")
                                 .fontWeight(.heavy)
-                                .foregroundColor(.yellow)
+                                .foregroundColor(isActiveBet10 ? Color("ColorYellow") : .white)
                                 .modifier(BetNumberModifier())
                         }
                         .modifier(BetCapsuleModifier())
